12dc9b64b67d310924fe0f90d15dbc45405a41b6
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-04-10 23:25:06 +0200

    root , Table & Mock_Data Components and Presenting Data

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './Components/Table.js'
+import id from './Data/Mock_Data.json'
+
+
+function App() {
+  return (
+    <>
+      <Table Data = {id}></Table>
+    </>
+  );
+}
+
+export default App;
