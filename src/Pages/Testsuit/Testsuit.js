4b29decdd6fa3b31da48bc297f84672985d770f7
shawky20 <shawky2500@gmail.com>
2023-04-15 17:31:04 +0200

    create statistics card

--- src/Pages/Testsuit/Testsuit.js
+++ src/Pages/Testsuit/Testsuit.js
@@ -2,10 +2,12 @@ import React from "react";
 import Table from "../../Components/Table/Table.js";
 import id from "../../Data/Mock_Data.json";
 import { Container } from "@mui/material";
+import StatisticCard from "../../Components/statistics/statistics"
 
 export default function Testsuit() {
   return (
     <Container>
+      <StatisticCard color={'blue'} ></StatisticCard>
       <Table Data={id}></Table>
     </Container>
   );
