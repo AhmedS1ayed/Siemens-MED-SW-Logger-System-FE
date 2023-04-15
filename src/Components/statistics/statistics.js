e4488ccfc8bf160cde1a01fae521bb05d392ae8f
shawky20 <shawky2500@gmail.com>
2023-04-15 18:31:42 +0200

    fetching JSON data and send it to a statistics card

--- src/Components/statistics/statistics.js
+++ src/Components/statistics/statistics.js
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types';
 
 
 const StatisticCard = ({ color, title, count }) => (
- 
     <div>
-        <h1>statistics </h1>
-        <p>color: {color}</p>
+        {/* data = {Data}/ */}
+      
+        {/* <p>color: {color}</p> */}
         <p>title: {title}</p>
         <p>count: {count}</p>
+        
 
     </div>
     
 );
 
 StatisticCard.propTypes = {
-    color: PropTypes.string,
+    color: PropTypes.string,    
     title: PropTypes.string,
-    count: PropTypes.string,
+
 };
 
 StatisticCard.defaultProps = {
--- src/Pages/Testsuit/Testsuit.js
+++ src/Pages/Testsuit/Testsuit.js
@@ -1,14 +1,34 @@
 import React from "react";
 import Table from "../../Components/Table/Table.js";
-import id from "../../Data/Mock_Data.json";
+import data from "../../Data/Mock_Data.json";
 import { Container } from "@mui/material";
 import StatisticCard from "../../Components/statistics/statistics"
 
+console.log(data.length)  
+
+let successfulCount = 0;
+let failedCount = 0;
+function getSuccessfulSuites(){
+  for(let i = 0; i < data.length; i++){
+    console.log("id ",data.id)
+    if(data.validationState == "Verified"){
+      successfulCount++;
+    }else{
+      failedCount++;
+    }
+  }
+
+}
 export default function Testsuit() {
   return (
+    getSuccessfulSuites(),
     <Container>
-      <StatisticCard color={'blue'} ></StatisticCard>
-      <Table Data={id}></Table>
+        <h1>statistics </h1>
+      <StatisticCard color={'blue'} title={'Total Test Suites'} count={data.length}  ></StatisticCard>
+      <StatisticCard color={'blue'} title={'Successful Test Suites'} count={successfulCount}  ></StatisticCard>
+      <StatisticCard color={'blue'} title={'Failed Test Suites'} count={failedCount}  ></StatisticCard>
+
+      <Table Data={data}></Table>
     </Container>
   );
-}
+} 
