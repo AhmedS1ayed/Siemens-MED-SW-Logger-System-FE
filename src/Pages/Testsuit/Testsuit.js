d2edae3ec3ef489c32a2883fc9d5a7d4c03bce1f
shawky20 <shawky2500@gmail.com>
2023-04-16 00:35:18 +0200

    add some styling to statisitcs component

--- src/Pages/Testsuit/Testsuit.js
+++ src/Pages/Testsuit/Testsuit.js
@@ -2,32 +2,29 @@ import React from "react";
 import Table from "../../Components/Table/Table.js";
 import data from "../../Data/Mock_Data.json";
 import { Container } from "@mui/material";
-import StatisticCard from "../../Components/statistics/statistics"
-
+import StatisticCard from "../../Components/statistics/StatisticsCard"
+import "../../Components/statistics/StatisticsCard.css"
 console.log(data.length)  
 
-let successfulCount = 0;
-let failedCount = 0;
-function getSuccessfulSuites(){
-  for(let i = 0; i < data.length; i++){
-    console.log("id ",data.id)
-    if(data.validationState == "Verified"){
-      successfulCount++;
-    }else{
-      failedCount++;
-    }
-  }
 
-}
+const totalTestSuites = data.length;
+const successfulTestSuites = data.filter((item) => item.validationState === 'Verified').length;
+const failedTestSuites = data.filter((item) => item.validationState === 'Failed').length;
+
+
 export default function Testsuit() {
-  return (
-    getSuccessfulSuites(),
-    <Container>
-        <h1>statistics </h1>
-      <StatisticCard color={'blue'} title={'Total Test Suites'} count={data.length}  ></StatisticCard>
-      <StatisticCard color={'blue'} title={'Successful Test Suites'} count={successfulCount}  ></StatisticCard>
-      <StatisticCard color={'blue'} title={'Failed Test Suites'} count={failedCount}  ></StatisticCard>
 
+  return (
+    <Container>
+      <h1>statistics </h1>
+      <div className="statistics-container">
+      
+      <StatisticCard color={'white'} title={'Total Test Suites'} count={totalTestSuites}  ></StatisticCard>
+      <StatisticCard color={'blue'} title={'Successful Test Suites'} count={successfulTestSuites}  ></StatisticCard>
+      <StatisticCard color={'blue'} title={'Failed Test Suites'} count={failedTestSuites}  ></StatisticCard>
+
+      </div>
+      <br></br>
       <Table Data={data}></Table>
     </Container>
   );
