c7f260aeaaf3a0c78da0777b71d45e62f1f16c25
ahmedbasha10 <bashamido6060@gmail.com>
2023-04-20 06:07:57 +0200

    add test case page & create links to test case for each test suit

---
+++ src/Components/DataGrid/DataGrid.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import "./DataGrid.css";
+
+export default function BasicExampleDataGrid({ data, data_columns }) {
+  return (
+    <div style={{ height: 800, width: "100%" }} className="dataGridContainer">
+      <DataGrid
+        columns={data_columns}
+        rows={data}
+        slots={{ toolbar: GridToolbar }}
+        getRowId={(row) => row.ID}
+        getRowClassName={() => "row-border"}
+        sx={{
+          boxShadow: 2,
+          paddingLeft: "40px",
+          paddingRight: "40px",
+          paddingTop: "20px",
+          borderColor: "primary.light",
+          marginTop: "40px",
+          fontWeight: "bold",
+        }}
+      />
+    </div>
+  );
+}
--- src/Pages/Testsuit/Testsuit.js
+++ src/Pages/Testsuit/Testsuit.js
@@ -1,12 +1,44 @@
 import React from "react";
 import { Container } from "@mui/material";
-
-import BasicExampleDataGrid from "../../Components/NewTable/NewTable.js";
+import DeleteIcon from "@mui/icons-material/Delete";
+import data from "../../Data/Mock_Data.json";
+import DataGrid from "../../Components/DataGrid/DataGrid.js";
+import { getColumnsName } from "../../Utils/utilities";
+import { Link } from "react-router-dom";
 
 export default function Testsuit() {
+  let data_columns = getColumnsName(data[0], { dateCreated: 250 });
+  data_columns.push({
+    field: "link",
+    headerName: "Link",
+    headerClassName: "super-app-theme--header",
+    width: 120,
+    renderCell: (params) => {
+      let testsuitId = params.id;
+
+      return <Link to={`/testcases?testsuitId=${testsuitId}`}>Show more</Link>;
+    },
+  });
+
+  data_columns.push({
+    field: "delete",
+    headerName: "",
+    width: 70,
+    headerClassName: "super-app-theme--header",
+    sortable: false,
+    filterable: false,
+    disableColumnMenu: true,
+    renderCell: (params) => (
+      <DeleteIcon
+        color="primary"
+        style={{ cursor: "pointer" }}
+        onClick={() => console.log(`Deleting row ${params.id}`)}
+      />
+    ),
+  });
   return (
     <Container>
-      <BasicExampleDataGrid />
+      <DataGrid data={data} data_columns={data_columns} />
     </Container>
   );
 }
