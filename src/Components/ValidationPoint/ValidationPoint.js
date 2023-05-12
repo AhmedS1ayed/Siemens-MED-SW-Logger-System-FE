23a4ed73ae555792724d7e9969f932fb2eaa43ce
ahmedbasha10 <bashamido6060@gmail.com>
2023-05-12 23:30:23 +0300

    create flip card

--- src/Components/ValidationPoint/ValidationPoint.js
+++ src/Components/ValidationPoint/ValidationPoint.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { CardContent, Dialog, Divider, Grid, Box } from "@mui/material";
+import {
+  CardContent,
+  Dialog,
+  Divider,
+  Grid,
+  Box,
+  Typography,
+} from "@mui/material";
 import TreeView from "@mui/lab/TreeView";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import TreeItem from "@mui/lab/TreeItem";
-import {Card, CardHeader} from "@mui/material";
+import { Card, CardHeader } from "@mui/material";
 import { getColumnName } from "../../Utils/utilities";
 import ExpandableRowTable from "../NewTable/NewTable";
+import "./ValidationPoint.css";
 
-export default function ValidaitonPoint({data, selectedRow}) {
+export default function ValidaitonPoint({ data, selectedRow }) {
+  // Lists to keep track of opened dialogs and fliped cards
   const [openDialogs, setOpenDialogs] = useState([]);
+  const [flipedCards, setFlipedCards] = useState([]);
+  // List contains name of keys that we want to expand by default inside tree
   const defaultExpanded = ["levels"];
   let columns = [];
 
   useEffect(() => {
     if (selectedRow !== -1) {
       setOpenDialogs(data[selectedRow]["validationPoints"].map(() => false));
+    }
+  }, [data, selectedRow]);
+
+  useEffect(() => {
+    if (selectedRow !== -1) {
+      setFlipedCards(
+        Array(data[selectedRow]["validationPoints"].length).fill(false)
+      );
     }
   }, [data, selectedRow]);
 
@@ -32,7 +51,8 @@ export default function ValidaitonPoint({data, selectedRow}) {
         sub_data !== "results" &&
         sub_data !== "isSuccessful" &&
         sub_data !== "parent" &&
-        sub_data !== "id"
+        sub_data !== "id" &&
+        sub_data !== "metaData"
       ) {
         if (typeof data[sub_data] === "object") {
           return (
@@ -60,6 +80,12 @@ export default function ValidaitonPoint({data, selectedRow}) {
         }
       }
     });
+  };
+
+  const flip = (idx) => {
+    const newFlipedCards = [...flipedCards];
+    newFlipedCards[idx] = !newFlipedCards[idx];
+    setFlipedCards(newFlipedCards);
   };
 
   return (
@@ -91,53 +117,112 @@ export default function ValidaitonPoint({data, selectedRow}) {
           data[selectedRow]["validationPoints"].map((valid_point, idx) => {
             return (
               <Grid item xs={12} sm={6} md={4} lg={4}>
-                <Card className="validation_point scale-up-center">
-                  <CardHeader
-                    className={`valid_point_header_${valid_point["isSuccessful"]}`}
-                    title={
-                      data[selectedRow]["metaData"]["name"] +
-                      Object.keys(valid_point["levels"]).map((level_key) => {
-                        return ` ${level_key} ${valid_point["levels"][level_key]} `;
-                      })
-                    }
-                  />
-                  <CardContent>
-                    <TreeView
-                      aria-label="file system navigator"
-                      defaultCollapseIcon={<ExpandMoreIcon />}
-                      defaultExpandIcon={<ChevronRightIcon />}
-                      defaultExpanded={defaultExpanded}
-                      sx={{
-                        height: 400,
-                        flexGrow: 1,
-                        maxWidth: "95%",
-                        overflowY: "auto",
-                      }}
-                    >
-                      {renderTree(valid_point)}
-                    </TreeView>
-                    <button
-                      className="results_btn"
-                      onClick={() => toggleDialog(idx)}
-                    >
-                      Results
-                    </button>
-                    <Dialog
-                      onClose={() => toggleDialog(idx)}
-                      open={openDialogs[idx]}
-                    >
-                      {valid_point["results"].forEach((result) =>
-                        getColumnName(result, columns)
-                      )}
-                      <ExpandableRowTable
-                        Data={valid_point["results"]}
-                        regularColumns={columns}
-                        expandable={false}
-                        onRowClickEnabled={false}
+                <div className={`card ${flipedCards[idx] ? "flip" : ""}`}>
+                  <div className="face front">
+                    <Card className="validation_point scale-up-center">
+                      <CardHeader
+                        className={`valid_point_header_${valid_point["isSuccessful"]}`}
+                        title={
+                          data[selectedRow]["metaData"]["name"] +
+                          Object.keys(valid_point["levels"]).map(
+                            (level_key) => {
+                              return ` ${level_key} ${valid_point["levels"][level_key]} `;
+                            }
+                          )
+                        }
                       />
-                    </Dialog>
-                  </CardContent>
-                </Card>
+                      <CardContent>
+                        <TreeView
+                          aria-label="file system navigator"
+                          defaultCollapseIcon={<ExpandMoreIcon />}
+                          defaultExpandIcon={<ChevronRightIcon />}
+                          defaultExpanded={defaultExpanded}
+                          sx={{
+                            height: 380,
+                            flexGrow: 1,
+                            maxWidth: "95%",
+                            overflowY: "auto",
+                          }}
+                        >
+                          {renderTree(valid_point)}
+                        </TreeView>
+                        <div className="buttons">
+                          <button
+                            className="results_btn"
+                            onClick={() => toggleDialog(idx)}
+                          >
+                            Results
+                          </button>
+                          <button
+                            className="metaData_btn"
+                            onClick={() => flip(idx)}
+                          >
+                            Meta Data
+                          </button>
+                        </div>
+
+                        <Dialog
+                          onClose={() => toggleDialog(idx)}
+                          open={openDialogs[idx]}
+                        >
+                          {valid_point["results"].forEach((result) =>
+                            getColumnName(result, columns)
+                          )}
+                          <ExpandableRowTable
+                            Data={valid_point["results"]}
+                            regularColumns={columns}
+                            expandable={false}
+                            onRowClickEnabled={false}
+                          />
+                        </Dialog>
+                      </CardContent>
+                    </Card>
+                  </div>
+                  <div className="face back">
+                    <div className="metaData">
+                      <Card>
+                        <CardHeader
+                          className={`valid_point_header_${valid_point["isSuccessful"]}`}
+                          title={
+                            data[selectedRow]["metaData"]["name"] +
+                            Object.keys(valid_point["levels"]).map(
+                              (level_key) => {
+                                return ` ${level_key} ${valid_point["levels"][level_key]} `;
+                              }
+                            )
+                          }
+                        />
+                        <CardContent>
+                          <Typography
+                            variant="h5"
+                            component="div"
+                            className="metaData_header"
+                          >
+                            Meta Data
+                          </Typography>
+                          <div className="metaData_body">
+                            {Object.keys(valid_point["metaData"]).map((key) => {
+                              return (
+                                <div className="metaData_content">
+                                  <div className="metaData_key">{key}</div>
+                                  <div className="metaData_value">
+                                    {valid_point["metaData"][key]}
+                                  </div>
+                                </div>
+                              );
+                            })}
+                          </div>
+                          <button
+                            className="metaData_btn"
+                            onClick={() => flip(idx)}
+                          >
+                            Back
+                          </button>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </div>
+                </div>
               </Grid>
             );
           })}
--- src/Pages/ValidationPoints/ValidationPoints.js
+++ src/Pages/ValidationPoints/ValidationPoints.js
@@ -1,108 +1,24 @@
 import React from "react";
-import { Container } from "@mui/material";
-import StatisticCard from "../../Components/statistics/StatisticsCard";
-import "../../Components/statistics/StatisticsCard.css";
-import { Link } from "react-router-dom";
-import ExpandableRowTable from "../../Components/NewTable/NewTable.js";
-import { useEffect, setData } from "react";
-import { useState } from "react";
-// import data from "../../Data/Mock_Data.json";
-import { getColumnName } from "../../Utils/utilities";
-import LinkIcon from "@mui/icons-material/Link";
+import "./ValidationPoints.css";
 
 export default function ValidationPoints() {
-  useEffect(() => {
-    fetch('http://localhost:8080/validationPoints')
-      .then(response => response.json())
-      .then(data => {
-        if(data)
-        {
-          setData(data.data);
-        }
-      })
-      .catch(error => console.error(error));
-  }, []);
-  
-  const [data, setData] = useState([
-    {
-      _id: "none",
-    },
-  ]);
-  
-  const totalTestSuites = data.length;
-  const successfulTestSuites = data.filter(
-    (item) => item.isSuccessful === true
-  ).length;
-  const failedTestSuites = data.filter(
-    (item) => item.isSuccessful === false
-  ).length;
-
-  const data_columns = [];
-  data.forEach((row) => getColumnName(row, data_columns));
-
-  data_columns.push({
-    name: "",
-    label: "",
-    options: {
-      filter: false,
-      sort: false,
-      customBodyRender: (value, tableMeta, updateValue) => {
-        const testsuitId = tableMeta.rowData[0];
-        return (
-          <Link to={`/testcases?testsuitId=${testsuitId}`}>
-            <LinkIcon />
-          </Link>
-        );
-      },
-    },
-  });
-
-  let count = 0;
-  data_columns.push({
-    name: "INDEX",
-    label: "INDEX",
-    options: {
-      filter: false,
-      sort: true,
-      customBodyRender: () => {
-       if(count === data.length) count = 0; 
-        count++;
-        return (count);
-      },
-    },
-  });
-  
   return (
-    <Container maxWidth="x">
-      {/* <h1>statistics</h1> */}
-      <div className="statistics-container">
-        <StatisticCard
-          title="Total Test Suites"
-          count={totalTestSuites}
-          color="#ffffff"
-          icon="equalizer"
-        />
-        <StatisticCard
-          title="Successful Test Suites"
-          count={successfulTestSuites}
-          color="#d4ead4"
-          icon="check"
-        />
-        <StatisticCard
-          title="Failed Test Suites"
-          count={failedTestSuites}
-          color="#f3d4d1"
-          icon="error"
-        />
+    <div className="card">
+      <div className="face front">
+        <h2>front</h2>
       </div>
-      <ExpandableRowTable
-        title="Test Suites"
-        Data={data}
-        regularColumns={data_columns}
-        expandable={false}
-        onRowClickEnabled={false}
-      />
-      <br />
-    </Container>
+      <div className="face back">
+        <h2>back</h2>
+      </div>
+    </div>
+    // <div class="container">
+    //   <div class="card">
+    //     <div class="front"></div>
+    //     <div class="back">
+    //       <h1>Back of Card</h1>
+    //       <p>Additional info on the back of the card</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
