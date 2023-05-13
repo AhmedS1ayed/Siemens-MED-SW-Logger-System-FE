0e35622b7613826910f1c52a9de14b5bc192211a
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-05-14 01:00:31 +0300

    fix theme button

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <ThemeButton />
+      <ThemeButton  />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Welcome />} />
